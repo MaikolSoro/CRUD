de7facc40d5c224ecbf1ae16a057de71329c3817
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-06-11 21:15:32 -0600

    Adding validation of text fields

---
+++ CRUD/Librerias/Numeros_separadores.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CRUD.Librerias
+{
+
+	internal static class Numeros_separadores
+	{
+
+		public static object Separador_de_Numeros(System.Windows.Forms.TextBox CajaTexto, System.Windows.Forms.KeyPressEventArgs e)
+		{
+			if (char.IsDigit(e.KeyChar))
+			{
+				e.Handled = false;
+			}
+			else if (char.IsControl(e.KeyChar))
+			{
+				e.Handled = false;
+			}
+			else if (e.KeyChar == '.' && (~CajaTexto.Text.IndexOf(".")) != 0)
+			{
+				e.Handled = true;
+			}
+			else if (e.KeyChar == '.')
+			{
+				e.Handled = false;
+			}
+			else if (e.KeyChar == ',')
+			{
+				e.Handled = false;
+
+			}
+			else
+			{
+				e.Handled = true;
+
+			}
+
+
+			return null;
+		}
+	}
+
+}
